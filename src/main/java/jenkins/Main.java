16b4c3e5790e11078e9bc61cf333a68ce49f5c7c
akankshavarshneyin@gmail.com <alookgupta@gmail.com>
2020-05-25 13:59:05 +0530

    first commit

---
+++ src/main/java/jenkins/Main.java
@@ -0,0 +1,12 @@
+package jenkins;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		System.out.println(add(10, 20));
+	}
+
+	public static int add(int x, int y) {
+		return x + y;
+	}
+}
---
+++ src/test/java/jenkins/Myapptest.java
@@ -0,0 +1,17 @@
+package jenkins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class Myapptest {
+	
+	@Test
+	public void addTest(){
+		
+		assertEquals(2, 2);
+	}
+	
+
+}
